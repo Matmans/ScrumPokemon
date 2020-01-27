f27b7bb95ebf1b32a90676fde73cc6445549e35d
Matty <matty.fieten@telenet.be>
2020-01-27 12:39:24 +0100

    uitbreiding model

--- src/app/shared/models/pokemon.model.ts
+++ src/app/shared/models/pokemon.model.ts
@@ -1,6 +1,9 @@
 export class Pokemon {
     constructor (
         public id: number,
-        public naam: string
+        public naam: string,
+        public sprite: string,
+        public type: string,
+        public type2: string
     ) {}
 }
