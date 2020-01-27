ef1635b3a1631546c9f368c80c5c143786dfb8f0
Michael <michael.rolle@hotmail.com>
2020-01-27 12:37:06 +0100

    pokemon service imports

--- src/app/shared/services/pokemon.service.ts
+++ src/app/shared/services/pokemon.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  url: string = 'https://pokeapi.co/api/v2/pokemon/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPokemon(id) : Observable<Pokemon[]>{
+    return this.http
+    .get<Pokemon[]>(this.url + `${id}`) 
+    .pipe()
+  }
+
 }
